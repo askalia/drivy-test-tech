575e6a0d861d1f9d3367934629096b543ff85563
Joris Grouillet <joris@askalia.net>
2019-05-31 23:20:41 +0200

    Level.computePrice() now is set abstract

--- core/level.js
+++ core/level.js
@@ -1,5 +1,3 @@
-const moment = require('moment');
-moment.suppressDeprecationWarnings = true;
 const fs = require('fs');
 const { normalize } = require('path');
 
@@ -28,18 +26,7 @@ class Level {
     }
 
     computePrice(rental, car) {
-        const operations = [
-            function computeTimeAspect(rental) {
-                const countRentalDuration = () => moment(rental.end_date).endOf("day").diff(moment(rental.start_date).startOf("day"), 'days') + 1;
-                return countRentalDuration() * car.price_per_day
-            },
-            function computeDistanceAspect(rental, car) { return rental.distance * car.price_per_km }
-        ];
-        return operations.reduce((totalPrice, op) => {
-            totalPrice += op(rental, car);
-            return totalPrice;
-        }, 0);
-
+        throw new Error('This method must be overriden');
     }
 
     findCarById(id) {
@@ -71,7 +58,7 @@ class Level {
         });
         return this._report;
     }
-    
+
     saveReport(outputFilePath, realm = null, callback = null) {
         const reportData = realm === null ? this.getRentalsReport() : { [realm] : this.getRentalsReport() };
         fs.writeFile(
--- level1/level1.js
+++ level1/level1.js
@@ -1,6 +1,23 @@
+const moment = require('moment');
+moment.suppressDeprecationWarnings = true;
+
 const Level = require('../core/level.js');
 
 class Level1 extends Level {
+    computePrice(rental, car) {
+        const operations = [
+            function computeTimeAspect(rental) {
+                const countRentalDuration = () => moment(rental.end_date).endOf("day").diff(moment(rental.start_date).startOf("day"), 'days') + 1;
+                return countRentalDuration() * car.price_per_day
+            },
+            function computeDistanceAspect(rental, car) { return rental.distance * car.price_per_km }
+        ];
+        return operations.reduce((totalPrice, op) => {
+            totalPrice += op(rental, car);
+            return totalPrice;
+        }, 0);
+
+    }
 }
 
 module.exports = Level1;
